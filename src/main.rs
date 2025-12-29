1d145517174742ea046fb8aed74629439c12e55e
Dario Saldia <dario.saldia@mbr-targeting.com>
2025-12-29 14:21:06 +0100

    Step 1 - simple parsing of arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::exit;
+
+fn parse_args() -> Result<(String, PathBuf), String> {
+    let usage = "Usage: rgrep <pattern> <path>".to_string();
+
+    let mut args = env::args();
+
+    args.next(); // Skip executable path
+
+    let pattern = args.next().ok_or(usage.clone())?;
+
+    let path = args.next().map(|s| PathBuf::from(s)).ok_or(usage)?;
+
+    Ok((pattern, path))
+}
+
 fn main() {
-    println!("Hello, world!");
+    match parse_args() {
+        Ok((pattern, path)) => {
+            println!("pattern: {} - path: {}", pattern, path.as_path().display());
+        }
+        Err(e) => {
+            eprintln!("{e}");
+            exit(2);
+        }
+    }
 }
