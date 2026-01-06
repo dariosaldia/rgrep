a1b3600089b3e7edb1c90ba7b0e73fdcf9bcc855
dariosaldia <dariosaldia@gmail.com>
2026-01-06 16:05:22 +0100

    feat(scanner): wrap sniff and rewind to a function

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,9 +1,9 @@
 use crate::sniff::is_text_file;
-use rayon::iter::IntoParallelRefIterator;
 use rayon::prelude::*;
+use std::fs::File;
+use std::io;
 use std::io::{BufRead, Seek};
 use std::path::{Path, PathBuf};
-use std::{fs, io};
 
 pub fn print_matches<F>(test_match: &F, files: &[PathBuf])
 where
@@ -12,6 +12,24 @@ where
     files.par_iter().for_each(|path| {
         scan_one_file(test_match, path.as_path());
     });
+}
+
+fn sniff_text_and_rewind(file: &mut File, path: &Path) -> bool {
+    match is_text_file(file) {
+        Err(e) => {
+            eprintln!("Error sniffing file {}. {}", path.display(), e);
+            false
+        }
+        Ok(false) => false,
+        Ok(true) => {
+            if let Err(e) = file.rewind() {
+                eprintln!("Error on file rewind {}. {}", path.display(), e);
+                false
+            } else {
+                true
+            }
+        }
+    }
 }
 
 fn scan_one_file<F>(test_match: &F, path: &Path)
@@ -26,18 +44,8 @@ where
         }
     };
 
-    match is_text_file(&mut file) {
-        Err(e) => {
-            eprintln!("Error sniffing file {}. {}", path.display(), e);
-            return;
-        }
-        Ok(false) => return,
-        Ok(true) => {
-            if let Err(e) = file.rewind() {
-                eprintln!("Error on file rewind {}. {}", path.display(), e);
-                return;
-            }
-        }
+    if !sniff_text_and_rewind(&mut file, path) {
+        return;
     }
 
     for line_attempt in io::BufReader::new(file).lines().enumerate() {
