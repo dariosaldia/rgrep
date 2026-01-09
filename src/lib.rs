958ed71d7c9af4afb5dfcfdc58ec2d2ebf7b632e
dariosaldia <dariosaldia@gmail.com>
2026-01-09 12:23:49 +0100

    WIP - move orchestration logic to lib and remove exit codes from main

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,40 @@
-pub mod sniff;
+use crate::scanner::print_matches;
+use crate::walker::collect_files;
+use regex::Regex;
+use std::path::PathBuf;
+
 pub mod scanner;
+pub mod sniff;
 pub mod walker;
 
+pub struct Config {
+    pub regex_mode: bool,
+    pub pattern: String,
+    pub path: PathBuf,
+}
+
+pub fn run(config: Config) -> i32 {
+    let files = match collect_files(&config.path) {
+        Ok(files) => files,
+        Err(e) => {
+            eprintln!("{e}");
+            return 2;
+        }
+    };
+
+    let matcher: Box<dyn Fn(&str) -> bool + Send + Sync> = if config.regex_mode {
+        let regex = match Regex::new(&config.pattern) {
+            Ok(regex) => regex,
+            Err(e) => {
+                eprintln!("Regex not valid: {}", e);
+                return 2;
+            }
+        };
+        Box::new(move |line| regex.is_match(line))
+    } else {
+        Box::new(move |line| line.contains(&config.pattern))
+    };
+
+    print_matches(matcher.as_ref(), &files);
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use regex::Regex;
-use rgrep::scanner::print_matches;
-use rgrep::walker::collect_files;
+use rgrep::{Config, run};
 use std::env;
 use std::path::PathBuf;
 use std::process::exit;
 
-fn parse_args() -> Result<(bool, String, PathBuf), String> {
+fn parse_args() -> Result<Config, String> {
     let usage = "Usage: rgrep [--regex] <pattern> <path>".to_string();
 
     let mut args = env::args();
@@ -28,38 +26,22 @@ fn parse_args() -> Result<(bool, String, PathBuf), String> {
     // read third argument
     let path = args.next().map(|s| PathBuf::from(s)).ok_or(usage)?;
 
-    Ok((regex_mode, pattern, path))
+    let config = Config {
+        regex_mode,
+        pattern,
+        path,
+    };
+
+    Ok(config)
 }
 
 fn main() {
-    let (regex_mode, pattern, path) = match parse_args() {
-        Ok((regex_mode, pattern, path)) => (regex_mode, pattern, path),
+    let config = match parse_args() {
+        Ok(config) => config,
         Err(e) => {
             eprintln!("{e}");
             exit(2);
         }
     };
-
-    let files = match collect_files(&path) {
-        Ok(files) => files,
-        Err(e) => {
-            eprintln!("{e}");
-            exit(2);
-        }
-    };
-
-    let matcher: Box<dyn Fn(&str) -> bool + Send + Sync> = if regex_mode {
-        let regex = match Regex::new(&pattern) {
-            Ok(regex) => regex,
-            Err(e) => {
-                eprintln!("Regex not valid: {}", e);
-                exit(2);
-            }
-        };
-        Box::new(move |line| regex.is_match(line))
-    } else {
-        Box::new(move |line| line.contains(&pattern))
-    };
-
-    print_matches(matcher.as_ref(), &files);
+    exit(run(config));
 }
